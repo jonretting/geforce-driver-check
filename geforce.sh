7f79f5ab187c0fd08fc089c1a2571425615205ad
Jon <jon@retting.com>
2013-11-28 09:41:11 -0800

    cleaups, fixes, checkonly option added, cleaned notes and comments

--- geforce.sh
+++ geforce.sh
@@ -10,9 +10,8 @@ DLHOST="http://us.download.nvidia.com" #use this mirror
 # binary dependency array
 DEPS=('PnPutil' 'wget' 'awk' 'cut' 'head' 'sed' 'wc' 'find' '7z' 'cygpath' 'ln' 'which')
 
-# clear vars *no edit
-
-LINK=
+# default vars
+LINK="http://www.nvidia.com/Download/processFind.aspx?psid=95&pfid=695&osid=19&lid=1&whql=&lang=en-us"
 FILEDATA=
 FILENAME=
 LATESTVER=
@@ -26,10 +25,13 @@ CWD=$PWD
 SEVENZIP=
 BINPATH=
 USE7ZPATH=false
+CHECKONLY=false
 ROOTPATH="/cygdrive/c"	#$(cygpath -W | sed -e "s/\/Windows//")
 EXTRACTSUBDIR=
 EXCLUDEPKGS="-xr!GFExperience* -xr!NV3DVision* -xr!Display.Update -xr!Display.Optimus -xr!MS.NET -xr!ShadowPlay"
 SETUPARGS="-nosplash -noeula -n"
+LATESTVERNAME= #friendly with decimal
+CURRENTVERNAME= #friendly with decimal
 
 error() { echo "Error: $1"; exit 1; }
 
@@ -79,16 +81,18 @@ Usage: geforce.sh [-s] [-y]
 Example: geforce.sh
 -s    Silent install (install new version without the Nvidia GUI)
 -y    Answer 'yes' to all prompts
+-C    Only check for new version (returns version#, 0=update available, 1=no update)
 -V    Displays version info
 -h    this crupt
 Version: ${VERSION}"
 }
 
-while getopts syhV OPTIONS; do
+while getopts syhVC OPTIONS; do
 	case "${OPTIONS}" in
 		s) SILENT=true	;;
 		y) YES=true	;;
 		V) usage | tail -n 1; exit 0	;;
+		C) CHECKONLY=true	;;
 		h) usage; exit 0	;;
 		*) usage; exit 1	;;
 	esac
@@ -117,24 +121,23 @@ if [[ $(echo "$REMOEMS" | wc -l) -gt 1 ]]; then
 	done
 fi
 
-# default nvidia starting link
-LINK="http://www.nvidia.com/Download/processFind.aspx?psid=95&pfid=695&osid=19&lid=1&whql=&lang=en-us"
-
 # file data query
 FILEDATA=$(wget -qO- "$(wget -qO- "$LINK" | awk '/driverResults.aspx/ {print $4}' | cut -d "'" -f2 | head -n 1)" | awk '/url=/ {print $2}' | cut -d '=' -f3 | cut -d '&' -f1)
 [[ $FILEDATA == *.exe ]] || error "Unexpected FILEDATA returned :: $FILEDATA"
 
-# store file name only
+# get file name only
 FILENAME=$(echo "$FILEDATA" | cut -d '/' -f4)
 [[ $FILENAME == *.exe ]] || error "Unexpected FILENAME returned :: $FILENAME"
 
-# store latest version
+# get latest version
 LATESTVER=$(echo "$FILEDATA" | cut -d '/' -f3 | sed -e "s/\.//")
 [[ $LATESTVER =~ ^[0-9]+$ ]] || error "LATESTVER not a number :: $LATESTVER"
+LATESTVERNAME=$(echo $LATESTVER| sed 's/./.&/4')
 
-# store current version
+# get current version
 CURRENTVER=$(PnPutil.exe -e | grep -C 2 "Display adapters" | grep -A 3 -B 1 "NVIDIA" | awk '/version/ {print $7}' | cut -d '.' -f3,4 | sed -e "s/\.//" | sed -r "s/^.{1}//")
 [[ $CURRENTVER =~ ^[0-9]+$ ]] || error "CURRENTVER not a number or multistring :: $CURRENTVER"
+CURRENTVERNAME=$(echo $CURRENTVER | sed 's/./.&/4')
 
 # old oem*.inf file if not already detected
 [[ -z $OLDOEMINF ]] && OLDOEMINF=$(PnPutil.exe -e | grep -C 2 "Display adapters" | grep -A 3 -B 1 "NVIDIA" | grep -B 3 "$(echo "$CURRENTVER" | sed 's/./.&/2')" | awk '/Published/ {print $4}')
@@ -144,22 +147,27 @@ CURRENTVER=$(PnPutil.exe -e | grep -C 2 "Display adapters" | grep -A 3 -B 1 "NVI
 DLURI="${DLHOST}${FILEDATA}"
 
 # check versions
-#[[ $LATESTVER -lt $CURRENTVER ]] && { echo "Already latest version: $(echo $CURRENTVER| sed 's/./.&/4')"; exit 0; }
+if [[ $LATESTVER -lt $CURRENTVER ]]; then
+	$CHECKONLY && exit 1
+	echo "Already latest version: $CURRENTVERNAME"
+	exit 0
+fi
+$CHECKONLY && { echo "$CURRENTVERNAME --> $LATESTVERNAME"; exit 0; }
 
 # run tasks
 echo -e "New version available!
-Current: $(echo $CURRENTVER | sed 's/./.&/4')
-Latest:  $(echo $LATESTVER | sed 's/./.&/4')
+Current: $CURRENTVERNAME
+Latest:  $LATESTVERNAME
 Downloading latest version into \"$DOWNLOADDIR\"...."
 cd "$DOWNLOADDIR" || error "Changing to download directory \"$DOWNLOADDIR\""
 wget -N "$DLURI" || error "Downloading file \"$DLURI\""
 
 # ask to isntall
-ask "Extract and Install new version ($LATESTVER) now?" || exit 0
+ask "Extract and Install new version ($LATESTVER) now?" || { echo "User cancelled"; exit 0; }
 
 # unarchive new version download
 [[ -d "${ROOTPATH}/NVIDIA" ]] || mkdir "${ROOTPATH}/MVIDIA" || error "creating directory :: \"$ROOTPATH/MVIDIA\""
-EXTRACTSUBDIR="${ROOTPATH}/NVIDIA/GDC-$(echo $LATESTVER | sed 's/./.&/4')"
+EXTRACTSUBDIR="${ROOTPATH}/NVIDIA/GDC-${LATESTVERNAME}"
 echo -ne "Extracting new driver archive..."
 [[ -d "$EXTRACTSUBDIR" ]] && rm -rf "$EXTRACTSUBDIR"
 7z x "$(cygpath -wap "${DOWNLOADDIR}/${FILENAME}")" -o"$(cygpath -wap "${EXTRACTSUBDIR}")" $EXCLUDEPKGS >/dev/null || error "extracting new download"
@@ -169,18 +177,14 @@ echo "Done"
 $SILENT && SETUPARGS+=" -s"
 
 # run the installer with args
+echo -ne "Executing installer setup..."
 cygstart -w "$EXTRACTSUBDIR/setup.exe" $SETUPARGS || error "Installation failed or user interupted!"
+echo "Done"
 
 # remove old oem inf package
 echo -ne "Removing old driver package..."
 PnPutil -d $OLDOEMINF >/dev/null || error "Removing old oem*.inf package (maybe in use):: $OLDOEMINF"
 echo "Done"
+
 echo "Driver installation successfull!"
-exit 0
-
-
-ask "Install new version ($LATESTVER) now?" &&
-cygstart -w "$FILENAME" 
-
-
 exit 0
