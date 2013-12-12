f9e9001f424dd608ee063f224dcf1b7489b158ff
Jon <jon@retting.com>
2013-12-11 16:23:13 -0800

    version bump

--- geforce.sh
+++ geforce.sh
@@ -19,7 +19,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-VERSION="1.0455"
+VERSION="1.0456"
 
 # cutomizable defaults
 DOWNLOAD_PATH=			# download data path (overides default windows\user\download path, but not inline "-d /path")
