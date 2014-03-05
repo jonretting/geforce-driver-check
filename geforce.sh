244872d2a0eecd4a8726a6371dd368d7e299065f
Jon <jon@retting.com>
2014-03-05 12:55:03 -0800

    vbump 1.06 beta

--- geforce.sh
+++ geforce.sh
@@ -19,7 +19,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-VERSION="1.0511"
+VERSION="1.06-BETA"
 
 # cutomizable defaults (respects environment defined vars) inline cmd over-rides both
 DOWNLOAD_PATH="${DOWNLOAD_PATH:=}"	# download path ex: DOWNLOAD_PATH="${DOWNLOAD_PATH:=/this/download/path}"
